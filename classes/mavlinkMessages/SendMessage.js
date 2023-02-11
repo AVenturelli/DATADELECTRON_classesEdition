7c70674821d234c609de256565f64f2d8c255d3e
Alberto Venturelli <albiventu95@hotmail.it>
2023-02-11 13:56:57 +0100

    Cambiamenti vari

--- classes/mavlinkMessages/SendMessage.js
+++ classes/mavlinkMessages/SendMessage.js
@@ -3,48 +3,43 @@ const Cesium = require("cesium");
 const Connection = require("../portConnection/Connection").Connection;
 
 class SendMessage {
-    constructor() {
-    }
-
-    static async setPlaneHome(lat, lon) {
-
-        const message = new common.CommandInt();
-        message.command = common.MavCmd.DO_SET_HOME;
-        message.current = 0;
-        message.targetSystem = 1;
-        message.targetComponent = 0;
-        message.param1 = 0;
-        message.param5 = Math.round(parseFloat(lat)*10000000);
-        message.param6 = Math.round(parseFloat(lon)*10000000);
-
-        let positions = [
-            Cesium.Cartographic.fromDegrees(
-                parseFloat(lon) ,
-                parseFloat(lat)
-            )
-        ];
-
-        let viewer = DatadCesium.getViewer();
-
-        let [updatedHeight] = await Promise.all([Cesium.sampleTerrain(viewer.terrainProvider, 12, positions)]);
-
-        message.param7 = Math.round(updatedHeight[0].height*1000);
-        //
-
-        console.log(message);
-
-        let serialPort = Connection.getSerialPort();
-        send(serialPort, message, new MavLinkProtocolV1())
-
-        ////////////////////////////7
-
-        const messageRequest = new common.CommandInt();
-        messageRequest.command = common.MavCmd.REQUEST_MESSAGE;
-        messageRequest.param1 = 242;
-
-        send(serialPort, messageRequest, new MavLinkProtocolV1())
-
-    }
+	constructor() {
+	}
+	static async setPlaneHome(lat, lon) {
+		
+		//Preparo il messaggio per settare la home
+		const message = new common.CommandInt();
+		message.command = common.MavCmd.DO_SET_HOME;
+		message.current = 0;
+		message.targetSystem = 1;
+		message.targetComponent = 0;
+		message.param1 = 0;
+		message.x = Math.round(parseFloat(lat) * 10000000);
+		message.y = Math.round(parseFloat(lon) * 10000000);
+		//Prendo l'altezza del suolo relativa alle coordinate grazie a cesium
+		let positions = [
+			Cesium.Cartographic.fromDegrees(
+				parseFloat(lon),
+				parseFloat(lat)
+			)
+		];
+		let viewer = DatadCesium.getViewer();
+		let [updatedHeight] = await Promise.all([Cesium.sampleTerrain(viewer.terrainProvider, 12, positions)]);
+		message.z = Math.round(updatedHeight[0].height * 1000);
+		//Preview del messaggio che sto per inivare
+		console.log(message);
+		//Mando il messaggio alla porta Seriale e alla Centralina
+		let serialPort = Connection.getSerialPort();
+		await send(serialPort, message, new MavLinkProtocolV1())
+		
+		////////////////////////////
+		//Mando il comando per richiedere la homePosition
+		const messageRequest = new common.CommandInt();
+		messageRequest.command = common.MavCmd.REQUEST_MESSAGE;
+		messageRequest.param1 = 242;
+		console.log(messageRequest);
+		await send(serialPort, messageRequest, new MavLinkProtocolV1())
+	}
 }
 
 exports.SendMessage = SendMessage
--- classes/portConnection/PortConnection.js
+++ classes/portConnection/PortConnection.js
@@ -7,168 +7,107 @@ const DatadSerialPort = require('./DatadSerialPorts.js').DatadSerialPorts
 
 
 class PortConnection {
-
-    #currentConnection
-    #currentPortName = undefined
-    #currentPortNameWrite
-    #currentBaudRate = undefined
-    #currentBaudRateWrite
-    #currentMavlinkReader
-    #currentMavlinkWriter
-    #connectionResult = false;
-    #flightData
-    #mavlinkParams
-
-    constructor() {
-
-        DatadSerialPort.listSerialPorts();
-
-        this.#createListeners();
-        this.#createClickers();
-        this.#createChangers();
-        this.#setIntervals();
-    }
-
-    #createListeners(){
-        ConnectionJQueryRenderer.openConnectionDataModal();
-    }
-
-    #createClickers() {
-
-        $('#port_refresh').click(() => {
-            DatadSerialPort.listSerialPorts();
-        })
-
-        $('#save_conn_param').click(() =>
-        {
-            this.#currentBaudRate = parseInt($("#baud_rate").val())
-            this.#currentPortName = $("#com_port").val()
-            this.#currentPortNameWrite = $("#com_port_out").val()
-            this.#currentBaudRateWrite = parseInt($("#baud_rate_out").val())
-
-            ConnectionJQueryRenderer.closeConnectionDataModal()
-
-            if(this.#currentPortName === null){
-
-                let modal = new CustomAlert(
-                    'connectionPortNotChosen',
-                    'ATTENZIONE!',
-                    'Non è stata scelta una porta seriale, non potrai effettuare una connessione!',
-                );
-                modal.printCode();
-                modal.showAlert();
-            }
-
-        })
-
-        $("#connect").click(() => {
-
-            if(Connection.getSerialPort() === undefined &&
-                this.#currentBaudRate !== undefined &&
-                this.#currentBaudRate !== null &&
-                this.#currentBaudRate !== "" &&
-                this.#currentPortName !== undefined &&
-                this.#currentPortName !== null &&
-                this.#currentPortName !== "") {
-
-                this.#doConnection();
-
-            }
-            else {
-                let modal = new CustomAlert(
-                    'connectionParamNotChosen',
-                    'ATTENZIONE!',
-                    'Non è stata scelta una porta seriale o non è stato scelto il baud rate',
-                );
-                modal.printCode();
-                modal.showAlert();
-            }
-        })
-
-        $("#disconnect").click(() => {
-            this.#currentMavlinkReader = undefined
-            this.#currentPortName = undefined
-            Connection.closeConnection()
-            ConnectionJQueryRenderer.setUiToNotConnected()
-        })
-    }
-
-    #createChangers(){
-        /*$("#customSwitch1").change(() =>
-        {
-            if($(this).is(':checked'))
-            {
-                $("#com_port_out").attr("disabled",false)
-                $("#baud_rate_out").attr("disabled",false)
-
-                this..write_data = true
-            }
-            else
-            {
-                $("#sendingDataFeedback").html("")
-                $("#sendingDataFeedback").hide()
-                $("#com_port_out").attr("disabled",true)
-                $("#baud_rate_out").attr("disabled",true)
-                globalThis.portnameWrite = undefined
-                globalThis.baudWrite = undefined
-                globalThis.write_data = false
-            }
-        })*/
-    }
-
-    #setIntervals(){
-
-        setInterval(() => {
-
-            if(!Connection.getSerialPort() !== undefined || (Connection.getSerialPort() !== undefined && Connection.checkIfConnected())) {
-                ConnectionJQueryRenderer.setUiToNotConnected()
-                //DatadSerialPort.listSerialPorts();
-            }
-            else {
-                ConnectionJQueryRenderer.setUiToConnected()
-            }
-        },2000)
-    }
-
-    #doConnection(){
-        try {
-
-            Connection.setPort(this.#currentPortName)
-            Connection.setBaud(this.#currentBaudRate)
-            Connection.connectToPort();
-            MavlinkReader.startReader();
-
-            /*this.#currentMavlinkReader = new MavlinkReader(
-                this.#currentConnection.connectToPort(),
-                this.#flightData,
-                this.#mavlinkParams
-            );
-
-            this.#currentMavlinkReader.startReader()*/
-
-
-
-            ConnectionJQueryRenderer.setUiToConnected();
-
-            //$("#zoom_range").attr("disabled",false)
-            //$("#current_zoom").attr("disabled",false)
-
-            /*if(globalThis.write_data && globalThis.portnameWrite !== undefined && globalThis.baudWrite !== undefined)
-            {
-                globalThis.currentConnectionWrite = new PortConnection(globalThis.portnameWrite,globalThis.baudWrite)
-                globalThis.currentWriter = new Writer(globalThis.currentConnectionWrite.connectToPort())
-                globalThis.currentWriter.startWriting()
-            }*/
-        } catch (error) {
-            //console.error("Errore: "+ error)
-            this.#currentConnection = undefined
-            let a = new CustomAlert('failedConnection',"CONNESSIONE FALLITA","La connessione alla porta "+this.#currentPortName+" con baud rate di "+this.#currentBaudRate+" è fallita con errore: <br><strong>"+error+"</strong>")
-            a.printCode()
-            a.showAlert()
-            //globalThis.#currentConnectionWrite = undefined
-            //globalThis.#currentWriter = undefined
-        }
-    }
+	
+	#currentConnection
+	#currentPortName = undefined
+	#currentPortNameWrite
+	#currentBaudRate = undefined
+	#currentBaudRateWrite
+	#currentMavlinkReader
+	#currentMavlinkWriter
+	#connectionResult = false;
+	#flightData
+	#mavlinkParams
+	
+	constructor() {
+		DatadSerialPort.listSerialPorts();
+		this.#createListeners();
+		this.#createClickers();
+		this.#setIntervals();
+	}
+	
+	#createListeners() {
+		ConnectionJQueryRenderer.openConnectionDataModal();
+	}
+	
+	#createClickers() {
+		
+		$('#port_refresh').click(() => {
+			DatadSerialPort.listSerialPorts();
+		})
+		
+		$('#save_conn_param').click(() => {
+			this.#currentBaudRate = parseInt($("#baud_rate").val())
+			this.#currentPortName = $("#com_port").val()
+			this.#currentPortNameWrite = $("#com_port_out").val()
+			this.#currentBaudRateWrite = parseInt($("#baud_rate_out").val())
+			ConnectionJQueryRenderer.closeConnectionDataModal()
+			if (this.#currentPortName === null) {
+				let modal = new CustomAlert(
+					'connectionPortNotChosen',
+					'ATTENZIONE!',
+					'Non è stata scelta una porta seriale, non potrai effettuare una connessione!',
+				);
+				modal.printCode();
+				modal.showAlert();
+			}
+		})
+		
+		$("#connect").click(() => {
+			
+			if (Connection.getSerialPort() === undefined &&
+				this.#currentBaudRate !== undefined &&
+				this.#currentBaudRate !== null &&
+				this.#currentBaudRate !== "" &&
+				this.#currentPortName !== undefined &&
+				this.#currentPortName !== null &&
+				this.#currentPortName !== "") {
+				this.#doConnection();
+			} else {
+				let modal = new CustomAlert(
+					'connectionParamNotChosen',
+					'ATTENZIONE!',
+					'Non è stata scelta una porta seriale o non è stato scelto il baud rate',
+				);
+				modal.printCode();
+				modal.showAlert();
+			}
+		})
+		
+		$("#disconnect").click(() => {
+			this.#currentMavlinkReader = undefined
+			this.#currentPortName = undefined
+			Connection.closeConnection()
+			ConnectionJQueryRenderer.setUiToNotConnected()
+		})
+	}
+	
+	#setIntervals() {
+		
+		setInterval(() => {
+			
+			if (Connection.getSerialPort() === undefined || (Connection.getSerialPort() !== undefined && !Connection.checkIfConnected())) {
+				ConnectionJQueryRenderer.setUiToNotConnected()
+			} else {
+				ConnectionJQueryRenderer.setUiToConnected()
+			}
+		}, 2000)
+	}
+	
+	#doConnection() {
+		try {
+			Connection.setPort(this.#currentPortName)
+			Connection.setBaud(this.#currentBaudRate)
+			Connection.connectToPort();
+			MavlinkReader.startReader();
+			ConnectionJQueryRenderer.setUiToConnected();
+		} catch (error) {
+			this.#currentConnection = undefined
+			let a = new CustomAlert('failedConnection', "CONNESSIONE FALLITA", "La connessione alla porta " + this.#currentPortName + " con baud rate di " + this.#currentBaudRate + " è fallita con errore: <br><strong>" + error + "</strong>")
+			a.printCode()
+			a.showAlert()
+		}
+	}
 }
 
 exports.PortConnection = PortConnection;
